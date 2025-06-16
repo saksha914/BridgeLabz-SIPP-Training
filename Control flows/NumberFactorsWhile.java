87ae02005f78e9d3efe7c22588eff3af9b714179
reet2003 <118253416+saksha914@users.noreply.github.com>
2025-06-16 09:43:26 +0530

    [Saksham Singh Bisht].Add Control Flows for day 3

---
+++ Control flows/NumberFactorsWhile.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class NumberFactorsWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+        
+        if (number <= 0) {
+            System.out.println("Please enter a positive integer.");
+        } else {
+            System.out.println("Factors of " + number + " are:");
+            int counter = 1;
+            while (counter <= number) {
+                if (number % counter == 0) {
+                    System.out.println(counter);
+                }
+                counter++;
+            }
+        }
+        
+        scanner.close();
+    }
+} 
